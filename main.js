63314c2b36f6df095c0c88dc5274e86934fb2e88
Rigidbody-JS <66162438+Zoro-JS2@users.noreply.github.com>
2023-06-05 09:54:18 +0300

    Add main files

---
+++ main.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import config from 'config';
+import { engine } from 'express-handlebars';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: config.get('OPENAI_KEY'),
+});
+const openai = new OpenAIApi(configuration);
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_, res) => {
+  res.render('index');
+});
+app.post('/', async (req, res) => {
+  const prompt = req.body.prompt;
+  const size = req.body.size ?? '512x512';
+  const number = req.body.number ?? 1;
+  if (prompt != '') {
+    try {
+      const response = await openai.createImage({
+        prompt,
+        size,
+        n: Number(number),
+      });
+      res.render('index', {
+        images: response.data.data,
+      });
+    } catch (e) {
+      res.render('index', {
+        error: e.message,
+      });
+    }
+  } else {
+    alert('Please input  a prompt');
+  }
+});
+app.listen(3000, () => console.log('Server started'));
